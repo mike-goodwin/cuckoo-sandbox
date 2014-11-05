8cf3ead456cdf5410f2ed937c310eaa295dff875
mike-goodwin <mike.goodwin@sage.com>
2014-11-05 23:22:50 +0000

    Create default.rb

---
+++ host/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: cuckoo sandbox
+# Recipe:: install
+# Author:: Mike Goodwin
+#
+# Copyright (C) 2014 Mike Goodwin
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# NB: This is under construction and does not work yet!
+#
+
+# Checkout, build and  pyssdeep
+
+execute 'checkout pyssdeep' do
+  command 'svn checkout http://pyssdeep.googlecode.com/svn/trunk/ /tmp/pyssdeep'
+end
+
+execute 'build pyssdeep' do
+  cwd '/tmp/pyssdeep'
+  command 'python /tmp/pyssdeep/setup.py build'
+  user 'root'
+end
+
+execute 'install pyssdeep' do
+  cwd '/tmp/pyssdeep'
+  command 'python /tmp/pyssdeep/setup.py install'
+  user 'root'
+end
+
+log 'pyssdeep downloaded and installed'
